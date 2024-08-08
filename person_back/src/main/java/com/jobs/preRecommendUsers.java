583b395a07823f5c2be60aba6076b3555e92faaa
柯垚 <14470559+ke-yao-yao@user.noreply.gitee.com>
2024-08-08 13:29:57 +0800

    通过定时任务实现数据预热

---
+++ person_back/src/main/java/com/jobs/preRecommendUsers.java
@@ -0,0 +1,46 @@
+package com.jobs;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.model.User;
+import com.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@Slf4j
+public class preRecommendUsers {
+    @Resource
+    private UserService userService;
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private List<Long> mainUserList = Arrays.asList(1L);
+
+    @Scheduled(cron = "0 0 1 * * ?")
+    public void preRecommendUsers() {
+        for (Long userId : mainUserList) {
+            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+            // 执行查询，获取用户列表
+            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+            // 获取当前用户
+            String redisKey = String.format("lack:user:recommend:%s", userId);
+            ValueOperations operations = redisTemplate.opsForValue();
+            // 缓存到redis
+            try {
+                operations.set(redisKey, userPage, 1, TimeUnit.HOURS);
+            } catch (Exception e) {
+                log.error("redis set key error", e);
+            }
+        }
+
+    }
+}
